cd53f281c86a02bc540f1dc9e30b4515cb9a8031
cjackson914 <63318622+cjackson914@users.noreply.github.com>
2020-10-05 20:48:31 -0400

    All files created. Mobile nav done

---
+++ dev/js/demo.js
@@ -0,0 +1,37 @@
+// import {gsap} from "gsap";
+
+
+/* -----------------
+    Demo Script
+----------------- */
+
+// var timeOfAnimation = 3;
+
+// gsap.from(".triangleyellow",{duration: timeOfAnimation, x: -1000, y: -1000, alpha: 0});
+// gsap.to(".triangleyellow",{duration: timeOfAnimation, x: 200, y: 300});
+
+// gsap.from(".triangleorange",{duration: timeOfAnimation, x: 2000, y: -325, alpha: 0});
+// gsap.to(".triangleorange",{duration: timeOfAnimation, x: 350, y: -325});
+
+// gsap.from(".rectangleblue",{duration: timeOfAnimation, x: 300, y: -1000, alpha: 0});
+// gsap.to(".rectangleblue",{duration: timeOfAnimation, x: 300, y: -50});
+
+// gsap.from(".linenavy1",{duration: timeOfAnimation, x: -1000, y: -100, alpha: 0});
+// gsap.to(".linenavy1",{duration: timeOfAnimation, x: 300, y: -100});
+
+// gsap.from(".linenavy2",{duration: timeOfAnimation, x: -1000, y: -450, alpha: 0});
+// gsap.to(".linenavy2",{duration: timeOfAnimation, x: 200, y: -450});
+
+// gsap.from(".lineorange",{duration: timeOfAnimation, x: 500, y: 2000, alpha: 0});
+// gsap.to(".lineorange",{duration: timeOfAnimation, x: 500, y: -250});
+
+
+ 
+
+
+/* <div class="triangleyellow"></div>
+<div class="triangleorange"></div>
+<div class="rectangleblue"></div>
+<div class="linenavy1"></div>
+<div class="linenavy2"></div>
+<div class="lineorange"></div> */
---
+++ dev/js/scripts.js
@@ -0,0 +1,18 @@
+// import {gsap} from "gsap";
+
+// import {triangleYellowAnimation} from "./triangleYellow.js";
+// import {triangleOrangeAnimation} from "./triangleOrange.js";
+// import {rectangleBlueAnimation} from "./rectangleBlue.js";
+// import {lineNavy1Animation} from "./lineNavy1.js";
+// import {lineNavy2Animation} from "./lineNavy2.js";
+// import {lineOrangeAnimation} from "./lineOrange.js";
+
+// const mainTL = gsap.timeline({paused:true});
+
+// mainTL.add(triangleYellowAnimation())
+//         .add(triangleOrangeAnimation())
+//         .add(rectangleBlueAnimation())
+//         .add(lineNavy1Animation())
+//         .add(lineNavy2Animation())
+//         .add(lineOrangeAnimation())
+//         .play();
